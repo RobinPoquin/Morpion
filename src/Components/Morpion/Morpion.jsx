5963a6408c9bba62abfec738debac9039c105df3
unknown <robpoquin@gmail.com>
2024-07-17 20:55:10 +0200

    Creation des styles de base et du plateau de jeu

--- src/Components/Morpion/Morpion.jsx
+++ src/Components/Morpion/Morpion.jsx
@@ -5,7 +5,31 @@ import cross_icon from '../Assets/cross.png'
 
 const Morpion = () => {
   return (
-    <div>Morpion</div>
+    <div className='container'>
+        <h1 className="title">
+            Morpion en <span>REACT</span>
+        </h1>
+        <div className="board">
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+    </div>
+        <button className="reset">
+            Recommencer
+        </button>
+    </div>
   )
 }
 
